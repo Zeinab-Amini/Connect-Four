5c14ac60f25f716c8c3a0c16398446b6dace3650
Zeinab <zeinab.amini13@gmail.com>
2022-10-11 16:50:48 -0500

    Update Connect four.py

--- Connect four.py
+++ Connect four.py
@@ -3,10 +3,10 @@ import pygame
 import sys
 import math
 
-BLUE =(0,0,255)
-BLACK=(0,0,0)
-RED=(255,0,0)
-YELLOW=(255,255,0)
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
 
 ROW_COUNT = 6
 COLUMN_COUNT = 7
